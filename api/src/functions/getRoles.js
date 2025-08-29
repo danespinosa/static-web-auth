c3f817f4acf670e7ecc23d4c545b74cd19da4916
Daniel Espinosa <luespino@microsoft.com>
2025-08-28 22:59:40 -0700

    minor changes

--- api/src/functions/getRoles.js
+++ api/src/functions/getRoles.js
@@ -8,6 +8,6 @@ app.http('getRoles', {
 
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { roles: ['authenticated'] };
+        return { roles: ['admin'] };
     }
 });
