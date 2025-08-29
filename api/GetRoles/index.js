16902ce109d563fbd10675f743cd78c6ab316a66
Daniel Espinosa <luespino@microsoft.com>
2025-08-28 22:16:57 -0700

    MODIFY to be able to login

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -9,12 +9,6 @@ const roleGroupMappings = {
 module.exports = async function (context, req) {
     const user = req.body || {};
     const roles = [];
-    
-    for (const [role, groupId] of Object.entries(roleGroupMappings)) {
-        if (await isUserInGroup(groupId, user.accessToken)) {
-            roles.push(role);
-        }
-    }
 
     context.res.json({
         roles
